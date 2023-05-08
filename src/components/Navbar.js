3ed4e6d1be34f09a72515bf990184d9466c9e3f3
Rahul Rai <rahulrai@founderandlightning.com>
2023-05-08 12:05:08 +0530

    Navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(props) {
         <>
             <nav className={`navbar navbar-expand-lg bg-${props.mode}`} data-bs-theme={props.mode}>
                 <div className="container-fluid">
-                    <Link className="nav-link active" to={`/`}>{props.title}</Link>
+                    <Link className={`nav-link active text-${props.mode === 'light' ? 'dark' : 'light'}`} to={`/`}>{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
