294fc5b3ccbf8cc66ece9f576f9c8098ced67621
Rahul Rai <rahulrai@founderandlightning.com>
2023-05-08 11:48:46 +0530

    Updates

---
+++ precache-manifest.9d2318062a20f5a5feee585abae3cf20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c09d7caf397eebbf99f",
+    "url": "/react-app/static/css/main.4fdd2fe3.chunk.css"
+  },
+  {
+    "revision": "3c09d7caf397eebbf99f",
+    "url": "/react-app/static/js/main.e22e2eb8.chunk.js"
+  },
+  {
+    "revision": "baeb97ecb51f8df8531c",
+    "url": "/react-app/static/js/runtime~main.c6028241.js"
+  },
+  {
+    "revision": "a318481572430ddd1772",
+    "url": "/react-app/static/js/2.a0b1730a.chunk.js"
+  },
+  {
+    "revision": "9e493117a3dfb5238748",
+    "url": "/react-app/static/js/3.f326d3da.chunk.js"
+  },
+  {
+    "revision": "46779674c7fb452187a625dee81fa217",
+    "url": "/react-app/index.html"
+  }
+];
